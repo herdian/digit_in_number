5a0864f555a1810dbc0e2dcf7012ab215e28197e
Herdian Sc <herdiansc@gmail.com>
2014-05-26 17:31:36 +0700

    Change return format, format return value inside the function

--- digit.js
+++ digit.js
@@ -1,9 +1,20 @@
-function findDigit(n,number,digit) {
+findDigit = function(n,number,digit) {
 	var number_length = number.toString().length;
 	digit = digit + number_length;
 	
 	if( digit >= n ) {
-		return {'digit':digit,'number':number};
+
+		var string_res_number = number.toString();
+		var margin = digit - process.argv[2];
+		var position = string_res_number.length-margin;
+		var digit_value = string_res_number[ position-1 ];
+	
+		return {
+			'n':n,
+			'answer':digit_value,
+			'position':position,
+			'number':number
+		};
 	}else {
 		return findDigit(n,number+1,digit);
 	}
@@ -11,9 +22,4 @@ function findDigit(n,number,digit) {
 
 var result = findDigit(process.argv[2],1,0);
 
-var string_res_number = result.number.toString();
-var margin = result.digit - process.argv[2];
-var position = string_res_number.length-margin;
-var digit_value = string_res_number[ position-1 ];
-
-console.log( 'Digit ke '+ process.argv[2] + ' adalah '+ digit_value +' angka ke '+ position +' pada angka '+ result.number);
+console.log( 'Digit ke '+ result.n + ' adalah '+ result.answer +' angka ke '+ result.position +' pada angka '+ result.number);
